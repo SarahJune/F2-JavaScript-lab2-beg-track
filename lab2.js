21bb9c51b6c753460d338c71fd081912141236d1
Sarah Fischer <sarah@codefellows.com>
2015-10-04 15:32:08 -0700

    lab 2 submission

--- lab2.js
+++ lab2.js
@@ -48,42 +48,34 @@ function assert(expression, failureMessage) {
 */
 
 function Blob(color, weight, eyes) {
-  this.color = color,
-  this.weight = weight,
-  this.eyes = eyes,
-  this.hoursToOoze
+  this.color = color;
+  this.weight = weight;
+  this.eyes = eyes;
 }
+// TODO: Next, create an instance of Blob named blob.
 
-/*
+var blob = new Blob('green', '200lb', '30');
 
- TODO: Next, create an instance of Blob named blob.
-
- */
-
-var blob("green", "200lb", "30") = new Blob
-
-/*
-
- TODO: Then, use a loop to calculate how long it took the blob to finish
- with Dowington. */
+// TODO: Then, use a loop to calculate how long it took the blob to finish
+// with Dowington.
 
 function calculateHours(population) {
   var myArray = [];
-  i = 0;
-  n = 0;
+  var i = 0;
+  var n = 0;
   while (n <= population) {
     n = n + i;
     myArray.push(n);
     i = i + 1;
-  };
+  }
   return myArray.length;
-};
+}
 
 // TODO: assign me the value of the
 // above calculation (how long it took
 // the blob to eat Dowington)
 
-var hoursSpentInDowington = calculateHours
+var hoursSpentInDowington = calculateHours;
 
 // Now, write a method that takes a population for an arbitrary
 // town, and the starting consumption rate, and returns the number
@@ -91,14 +83,14 @@ var hoursSpentInDowington = calculateHours
 
 Blob.prototype.hoursToOoze = function(population, peoplePerHour) {
   var myArray = [];
-    hours = 0;
-    eaten = peoplePerHour;
-    while (eaten < population) {
-      eaten = eaten + hours;
-      myArray.push(eaten);
-      hours = hours + 1;
-    };
-    return myArray.length;
+  var hours = 0;
+  var eaten = peoplePerHour;
+  while (eaten < population) {
+    eaten = eaten + hours;
+    myArray.push(eaten);
+    hours = hours + 1;
+  }
+  return myArray.length;
 };
 
 assert(blob.hoursToOoze(0, 1) === 0, 'no people means no time needed.');
@@ -109,7 +101,7 @@ assert(blob.hoursToOoze(1000, 1) === hoursSpentInDowington,
 // the hoursToOoze method.
 
 assert(blob.hoursToOoze(500, 5) === 32,
-  'answer should be 32)';
+  'answer should be 32');
 assert(blob.hoursToOoze(750, 750) === 0,
   'answer should be 0. It takes no time at all for a big ass blob to eat 750 people.');
 assert(blob.hoursToOoze(0, 0) === 0,
@@ -130,34 +122,42 @@ var hello = {
 // speak, and method (that you'll place on the prototype) called
 // sayHello.
 
-function SentientBeing (home, language) {
-  this.homePlanet = home,
-  this.language = language,
+function SentientBeing(home, language) {
+  this.homePlanet = home;
+  this.language = language;
 }
 
 // sb is a SentientBeing object
-function sayHello (sb) {
-  console.log(hello.language)
-  return hello(sb)
-
-    // TODO: say hello prints out (console.log's) hello in the
-    // language of the speaker, but returns it in the language
-    // of the listener (the sb parameter above).
-    // use the 'hello' object at the beginning of this exercise
-    // to do the translating
-
-    //TODO: put this on the SentientBeing prototype
-    SentientBeing.prototype.sayHello()
-  }
+function sayHello(sb) {
+  // TODO: say hello prints out (console.log's) hello in the
+  // language of the speaker, but returns it in the language
+  // of the listener (the sb parameter above).
+  // use the 'hello' object at the beginning of this exercise
+  // to do the translating
+  console.log(hello[this.language]);
+  return hello.sb;
+}
+  //TODO: put this on the SentientBeing prototype
+SentientBeing.prototype.sayHello();
 
 // TODO: create three SentientBeings, one for each language in the
 // 'hello' object above.
-var klingon = new SentientBeing("Qo\'noS", ); // TODO: make a klingon
-var romulan = new SentientBeing(); // TODO: make a romulan
-var human = new SentientBeing(); // TODO: make a human
+var klingon = new SentientBeing('Qo\'noS', 'klingon'); // TODO: make a klingon
+var romulan = new SentientBeing('Jolan\'tru', 'Romulus'); // TODO: make a romulan
+var human = new SentientBeing('federation standard', 'Earth'); // TODO: make a human
 
 assert(human.sayHello(klingon) === 'nuqneH',
   'the klingon should hear nuqneH');
+assert(klingon.sayHello(human) === 'hello',
+  'the human should hear hello');
+assert(romulan.sayHello(klingon) === 'nuqneH',
+  'the klingon should hear nuqneH');
+assert(klingon.sayHello(romulan) === 'Jolan\'tru',
+  'the romulan should hear Jolan\'tru');
+assert(human.sayHello(romulan) === 'Jolan\'tru',
+  'the romulan should hear Jolan\'tru');
+assert(romulan.sayHello(human) === 'hello',
+  'the human should hear hello');
 
 // TODO: write five more assertions, to complete all the possible
 // greetings between the three types of sentient beings you created above.
@@ -168,14 +168,20 @@ assert(human.sayHello(klingon) === 'nuqneH',
 // Implement the following functions. Write at least 3
 // assertions for each one
 //*********************************************************
-function max(array) {
   // TODO: return the largest number in the given array
+var myArray = [1, 2, 3];
+function max(array) {
+  return Math.max.apply(Math, array);
 }
+console.log(max(myArray));
+// props to this explanation of Math.math.apply: http://stackoverflow.com/questions/1379553/how-might-i-find-the-largest-number-contained-in-a-javascript-array
 
 // TODO: write three more assertions
-assert(max([ 1, 3, 2 ]) === 3, '[1,3,2]');
+assert(max([1, 3, 2]) === 3, '[1,3,2]');
+assert(max([425, 333]) === 425, '425');
+assert(max([425, 333]) === 333, 'NOPE');
+assert(max([1, 1]) === 1, 'ALSO NOPE');
 
-function variablify(string) {
   // TODO: you are given a string with several words in it
   // return a corresponding variable name that follows
   // javascript conventions
@@ -183,8 +189,19 @@ function variablify(string) {
   // you might want to use these string methods:
   //  split(), charAt(), toUpperCase()
   // and this array method: join()
+var myString = 'KuNg foO MaSTeR';
+function variablify(string) {
+  var newArray = [];
+  newArray = string.toLowerCase().split(' ');
+  var str = [];
+  for (var i = 0; i < newArray.length; i++) {
+    str.push(newArray[i].charAt(0).toUpperCase() + newArray[i].slice(1, newArray[i].length));
+  }
+  newArray = str.join('');
+  return newArray.charAt(0).toLowerCase() + newArray.slice(1, newArray.length);
 }
 
+console.log(variablify(myString));
 // TODO: write three more assertions
 assert(variablify('one two three') === 'oneTwoThree',
   'variablify(\'one two three\')');
